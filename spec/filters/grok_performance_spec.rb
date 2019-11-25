c88deae119e7f1b3776464273ba2b9ef4578e2ea
Karol Bucek <self@kares.org>
2019-11-25 17:34:26 +0100

    Test: avoid using pipeline in performance test

--- spec/filters/grok_performance_spec.rb
+++ spec/filters/grok_performance_spec.rb
@@ -13,6 +13,10 @@ require "logstash/filters/grok"
 
 describe LogStash::Filters::Grok do
 
+  subject do
+    described_class.new(config).tap { |filter| filter.register }
+  end
+
   describe "base-line performance", :performance => true do
 
     EVENT_COUNT = 300_000
@@ -24,27 +28,17 @@ describe LogStash::Filters::Grok do
     # logstash_1_d010d1d29244 | filters/grok parse rate: 29957/sec, elapsed: 10.014199s
     # logstash_1_d010d1d29244 | filters/grok parse rate: 32932/sec, elapsed: 9.109601999999999s
 
-    config <<-CONFIG
-      input {
-        generator {
-          count => #{EVENT_COUNT}
-          message => "Mar 16 00:01:25 evita postfix/smtpd[1713]: connect from camomile.cloud9.net[168.100.1.3]"
-        }
-      }
-      filter {
-        grok {
-          match => { "message" => "%{SYSLOGLINE}" }
-          overwrite => [ "message" ]
-        }
-      }
-      output { null { } }
-    CONFIG
+    let(:config) do
+      { 'match' => { "message" => "%{SYSLOGLINE}" }, 'overwrite' => [ "message" ] }
+    end
 
     it "matches at least #{EXPECTED_MIN_RATE} events/second" do
       max_duration = EVENT_COUNT / EXPECTED_MIN_RATE
-      pipeline = new_pipeline_from_string(config)
+      message = "Mar 16 00:01:25 evita postfix/smtpd[1713]: connect from camomile.cloud9.net[168.100.1.3]"
       expect do
-        duration = measure { pipeline.run }
+        duration = measure do
+          EVENT_COUNT.times { subject.filter(LogStash::Event.new("message" => message)) }
+        end
         puts "filters/grok parse rate: #{"%02.0f/sec" % (EVENT_COUNT / duration)}, elapsed: #{duration}s"
       end.to perform_under(max_duration).warmup(1).sample(2).times
     end
@@ -56,55 +50,67 @@ describe LogStash::Filters::Grok do
     ACCEPTED_TIMEOUT_DEGRADATION = 7.5 # in % (compared to timeout-less run)
     # NOTE: usually bellow 5% on average -> we're expecting every run to be < (+ given%)
 
+    MATCH_PATTERNS = {
+      "message" => [
+        "foo0: %{NUMBER:bar}", "foo1: %{NUMBER:bar}", "foo2: %{NUMBER:bar}", "foo3: %{NUMBER:bar}", "foo4: %{NUMBER:bar}",
+        "foo5: %{NUMBER:bar}", "foo6: %{NUMBER:bar}", "foo7: %{NUMBER:bar}", "foo8: %{NUMBER:bar}", "foo9: %{NUMBER:bar}",
+        "%{SYSLOGLINE}"
+      ]
+    }
+
+    SAMPLE_MESSAGE = "Mar 16 00:01:25 evita postfix/smtpd[1713]: connect from aaaaaaaa.aaaaaa.net[111.111.11.1]".freeze
+
+    let(:config_wout_timeout) do
+      {
+        'match' => MATCH_PATTERNS,
+        'timeout_millis' => 0   # 0 - disabled timeout
+      }
+    end
+
+    let(:config_with_timeout) do
+      {
+        'match' => MATCH_PATTERNS,
+        'timeout_scope' => "event",
+        'timeout_millis' => 10_000
+      }
+    end
+
     it "has less than #{ACCEPTED_TIMEOUT_DEGRADATION}% overhead" do
-      no_timeout_config = <<-CONFIG
-        input {
-          generator {
-            count => 500000
-            message => "Mar 16 00:01:25 evita postfix/smtpd[1713]: connect from aaaaaaaa.aaaaaa.net[111.111.11.1]"
-          }
-        }
-        filter {
-          grok {
-            match => { "message" => [ 
-              "foo0: %{NUMBER:bar}", "foo1: %{NUMBER:bar}", "foo2: %{NUMBER:bar}", "foo3: %{NUMBER:bar}", "foo4: %{NUMBER:bar}",
-              "foo5: %{NUMBER:bar}", "foo6: %{NUMBER:bar}", "foo7: %{NUMBER:bar}", "foo8: %{NUMBER:bar}", "foo9: %{NUMBER:bar}",
-              "%{SYSLOGLINE}" 
-            ] }
-            timeout_scope => "pattern"
-            timeout_millis => 0   # 0 - disabled timeout
-          }
-        }
-        output { null { } }
-      CONFIG
-
-      timeout_config = no_timeout_config.
-          sub('timeout_scope => "pattern"', 'timeout_scope => "event"').
-          sub('timeout_millis => 0', 'timeout_millis => 10000')
-
-      no_timeout_pipeline = new_pipeline_from_string(no_timeout_config)
-      no_timeout_duration = measure { no_timeout_pipeline.run } # warmup
-      puts "filters/grok(timeout => 0) warmed up in #{no_timeout_duration}"
+      filter_wout_timeout = LogStash::Filters::Grok.new(config_wout_timeout).tap(&:register)
+      wout_timeout_duration = measure { filter_wout_timeout.filter(sample_event) } # warmup
+      puts "filters/grok(timeout => 0) warmed up in #{wout_timeout_duration}"
       gc!
       no_timeout_durations = Array.new(3).map do
-        duration = measure { no_timeout_pipeline.run }
+        duration = measure do
+          for _ in (1..EVENT_COUNT) do # EVENT_COUNT.times without the block cost
+            filter_wout_timeout.filter(sample_event)
+          end
+        end
         puts "filters/grok(timeout => 0) took #{duration}"
         duration
       end
 
-      timeout_pipeline = new_pipeline_from_string(timeout_config)
-      timeout_duration = measure { timeout_pipeline.run } # warmup
-      puts "filters/grok(timeout_scope => event) warmed up in #{timeout_duration}"
+      filter_with_timeout = LogStash::Filters::Grok.new(config_with_timeout).tap(&:register)
+      with_timeout_duration = measure { filter_with_timeout.filter(sample_event) } # warmup
+      puts "filters/grok(timeout_scope => event) warmed up in #{with_timeout_duration}"
 
       expected_duration = avg(no_timeout_durations)
       expected_duration += (expected_duration / 100) * ACCEPTED_TIMEOUT_DEGRADATION
       puts "expected_duration #{expected_duration}"
       gc!
       expect do
-        duration = measure { timeout_pipeline.run }
+        duration = measure do
+          for _ in (1..EVENT_COUNT) do # EVENT_COUNT.times without the block cost
+            filter_with_timeout.filter(sample_event)
+          end
+        end
         puts "filters/grok(timeout_scope => event) took #{duration}"
         duration
       end.to perform_under(expected_duration).sample(3).times
+    end
+
+    def sample_event
+      LogStash::Event.new("message" => SAMPLE_MESSAGE)
     end
 
   end
